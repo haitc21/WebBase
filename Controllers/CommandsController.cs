f0fbb109a821f1a3aa695328218ee02823d02eda
haitc <haitc@HAITC_DES>
2021-06-22 11:41:58 +0700

    Commands

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebBase.Data;
+using WebBase.Models.ViewModels;
+
+namespace WebBase.Controllers
+{
+    public class CommandsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommandsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetCommants()
+        {
+            var commands = _context.Commands;
+
+            var commandVms = await commands.Select(u => new CommandVM()
+            {
+                Id = u.Id,
+                Name = u.Name,
+            }).ToListAsync();
+
+            return Ok(commandVms);
+        }
+    }
+}
