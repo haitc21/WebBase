842a4a31caa467416685f9bdf38b7ed96cbbc8f1
haitc <haitc@LAPTOP-TV8IJN76>
2021-10-18 21:37:38 +0700

    complete

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -144,13 +144,48 @@ namespace WebBase.Controllers
 
             var existingPermissions = _context.Permissions.Where(x => x.RoleId == roleId);
             _context.Permissions.RemoveRange(existingPermissions);
-            _context.Permissions.AddRange(newPermissions);
+            _context.Permissions.AddRange(newPermissions.Distinct(new MyPermissionComparer()));
             var result = await _context.SaveChangesAsync();
             if (result > 0)
             {
                 return NoContent();
             }
             return BadRequest(new ApiBadRequestResponse("Update permission failed!"));
+        }
+    }
+    /// <summary>
+    /// So sanh 2 đối tượng permission
+    /// giống như trên code dạo
+    /// khái niệm deep comparer
+    /// </summary>
+    internal class MyPermissionComparer : IEqualityComparer<Permission>
+    {
+        // Items are equal if their ids are equal.
+        public bool Equals(Permission x, Permission y)
+        {
+            // Check whether the compared objects reference the same data.
+            if (Object.ReferenceEquals(x, y)) return true;
+
+            // Check whether any of the compared objects is null.
+            if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null))
+                return false;
+
+            //Check whether the items properties are equal.
+            return x.CommandId == y.CommandId && x.FunctionId == x.FunctionId && x.RoleId == x.RoleId;
+        }
+
+        // If Equals() returns true for a pair of objects
+        // then GetHashCode() must return the same value for these objects.
+
+        public int GetHashCode(Permission permission)
+        {
+            //Check whether the object is null
+            if (Object.ReferenceEquals(permission, null)) return 0;
+
+            //Get hash code for the ID field.
+            int hashProductId = (permission.CommandId + permission.FunctionId + permission.RoleId).GetHashCode();
+
+            return hashProductId;
         }
     }
 }
